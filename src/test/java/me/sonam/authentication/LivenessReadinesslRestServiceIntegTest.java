bc383764c11279bbe3d8db7579ce1fb4221a6d60
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2023-09-24 20:16:27 -0600

    fix path for health

--- src/test/java/me/sonam/authentication/LivenessReadinesslRestServiceIntegTest.java
+++ src/test/java/me/sonam/authentication/LivenessReadinesslRestServiceIntegTest.java
@@ -1,12 +1,16 @@
 package me.sonam.authentication;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -21,6 +25,8 @@ public class LivenessReadinesslRestServiceIntegTest {
 
   @Autowired
   private WebTestClient client;
+  @MockBean
+  private ReactiveJwtDecoder jwtDecoder;
 
   @Test
   public void readiness() {
--- src/test/java/me/sonam/authentication/PermitPathLivenessCheck.java
+++ src/test/java/me/sonam/authentication/PermitPathLivenessCheck.java
@@ -3,12 +3,16 @@ package me.sonam.authentication;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -21,6 +25,8 @@ public class PermitPathLivenessCheck {
     private static final Logger LOG = LoggerFactory.getLogger(PermitPathLivenessCheck.class);
     @Autowired
     private WebTestClient webTestClient;
+    @MockBean
+    private ReactiveJwtDecoder jwtDecoder;
 
     @Test
     public void liveness() {
