52d257aecc2f53c5b1a810b86708bbb742467c94
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2025-11-03 20:29:17 -0700

    remove drop table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,2 @@
-drop table Authentication if exists;
-
 CREATE TABLE if not exists Authentication (authentication_id varchar PRIMARY KEY, password varchar, user_id UUID,
   signin_source_id UUID, active boolean, access_date_time timestamp);
