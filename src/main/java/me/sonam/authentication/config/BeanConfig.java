0ab862ed9089c856a2c67456f42eac03438140e9
sonamsamduphangsar <sonamhava@gmail.com>
2026-06-26 16:02:56 -0600

    Split WebClient configs by Eureka and non-Eureka profiles

--- src/main/java/me/sonam/authentication/config/BeanConfig.java
+++ src/main/java/me/sonam/authentication/config/BeanConfig.java
@@ -1,6 +1,8 @@
 package me.sonam.authentication.config;
 
+import me.sonam.authentication.handler.SimpleAuthenticationService;
 import me.sonam.authentication.webclient.RoleWebClient;
+import me.sonam.security.headerfilter.ReactiveRequestContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +15,9 @@ public class BeanConfig {
     @Value("${role-rest-service.context}")
     private String roleEndpoint;
 
+    @Value("${tokenExpireSeconds:1}")
+    private int tokenExpireSeconds;
+
     @Autowired
     @Qualifier("serviceWebClientBuilder")
     private WebClient.Builder serviceWebClientBuilder;
@@ -20,5 +25,17 @@ public class BeanConfig {
     @Bean
     public RoleWebClient roleWebClient() {
         return new RoleWebClient(serviceWebClientBuilder, roleEndpoint);
+    }
+
+    @Bean
+    public ReactiveRequestContextHolder reactiveRequestContextHolder(
+            @Qualifier("tokenWebClientBuilder") WebClient.Builder tokenWebClientBuilder) {
+        return new ReactiveRequestContextHolder(tokenWebClientBuilder, tokenExpireSeconds);
+    }
+
+    @Bean
+    public SimpleAuthenticationService simpleAuthenticationService(
+            @Qualifier("serviceWebClientBuilder") WebClient.Builder serviceWebClientBuilder) {
+        return new SimpleAuthenticationService(serviceWebClientBuilder);
     }
 }
